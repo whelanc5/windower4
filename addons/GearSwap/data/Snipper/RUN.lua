a945ebe72584794900e2d08ec25214b7daa4f53d
whelanc5 <cdwhelan@gmail.com>
2017-09-08 14:27:44 -0400

    RUN

---
+++ addons/GearSwap/data/Snipper/RUN.lua
@@ -0,0 +1,97 @@
+function get_sets() 
+	-- Commands in game
+	-- /gs c changeTP  switches between the list of modes in tpModes, equips the gear and sets it to current aftercast set
+	-- /gs c "setName" quips the gear set and sets it to current aftercast set
+	-- /gs c deploy puts autodeploy on, when you engage it will cast deploy
+	-- /gs c automaneuver turns on automaneuver, when maneuvers wear it recasts them
+	--/ in my init file (windower/scripts/init.txt) f9 is bound to sent /gs c changeTP 
+	
+	
+	include('cdhelper.lua')
+
+
+
+----------------------------------------------------------------------Base sets-------------------------------------------------------------------------------
+
+	
+	modeSets["tpMode"].setModes = {"Default", "Acc", "Haste","Custom"} -- These are the sets that will cycle Modes, just make sure the set matches the name here ex: sets.TP.Name will equip if "Name" is in this list
+	modeSets["dtMode"].setModes = {"Default", "Magic", "Full", "Custom"} --sets.DT.Mode
+	modeSets["wsMode"].setModes = {"Default", "Acc"}  --sets.WS.Mode
+	modeSets["idleMode"].setModes = {"Default","Custom"}
+	
+	-- --- precast--------------------------------
+	sets.precast = {}
+	sets.precast.Magic = {}
+	sets.precast.TP ={}			
+	sets.precast.WS = {}
+	sets.precast.JA = {}
+	sets.precast.Magic = {}
+	--------------midcast----------------------
+	sets.midcast = {}
+	------------------aftercast----------------
+	sets.aftercast = {}	
+	--------------blue-------------------------
+	sets.Blue = {}
+	sets.Blue.Magic = {}
+	sets.Blue.Physical = {}
+	sets.Blue.Debuff = {}
+	sets.Blue.Buff = {}
+	sets.Blue.Cure = {}
+	------------Nuke-------------------------
+	sets.Nuke = {}
+
+
+	sets.base = {head="Herculean Helm",neck="Lissome Necklace",
+        ear1="Bladeborn Earring",ear2="Steelflash Earring",body="Tali'ah Manteel",hands="Herculean Gloves",
+        ring1="Rajas Ring",ring2="Epona's Ring", back="", waist ="Hurch'lan Sash",legs= hercLegsTA,
+        feet= hercFeetTA}
+	--------------------------------------------------WS-----------------------------------------------------------------------
+	--sets.base.WS = set_combine(sets.base,{ head="Hizamaru Somen +1", hands="Ryuo Tekko",  legs="Hiza. Hizayoroi +1", body = "Hizamaru Haramaki"})	
+	sets.WS = set_combine(sets.base,{ })
+	sets.WS.Default = set_combine(sets.WS,{})
+	sets.WS.Acc = set_combine(sets.WS,{ })	
+    sets.precast.WS = sets.WS -- don't change
+	
+
+
+
+	-----------------------------------------------Tp---------------------------------------------------------------------------------------
+    sets.TP = set_combine(sets.base,{})
+	sets.aftercast.TP = sets.TP  
+	sets.TP.Current = sets.TP
+	sets.TP.Default = set_combine(sets.TP,{})
+    sets.TP.Acc = set_combine(sets.TP,{})	
+	sets.TP.Haste = set_combine(sets.TP,{})	
+    sets.TP.DT=  set_combine(sets.base,{neck="Twilight Torque", ring1="Dark Ring", ring2="Dark Ring"})
+
+	sets.TP.Custom = set_combine(sets.TP,{})
+	
+	--------------------------------------------------------------------JA------------------------------------------------------------------
+
+	
+	
+	------------------------------------------DT-----------------------------------------------------------------------------------------------
+	sets.DT = set_combine(sets.base,{neck="Twilight Torque", ring1="Dark Ring", ring2="Dark Ring"})
+    sets.DT.Default = set_combine(sets.base,{})
+	sets.DT.Magic  = set_combine(sets.base,{})
+	sets.DT.Full = sets.DT
+	sets.DT.Custom = sets.DT
+	
+	------------------------------------------------------After Cast---------------------------------------------
+    
+  
+	
+	----------------------------------------------------Idle------------------------------------------------------------------
+	sets.Idle = set_combine(sets.DT,{feet="Hermes' Sandals"})
+	sets.Idle.Current = sets.Idle
+	sets.Idle.DT = set_combine(sets.DT,{})
+	sets.Idle.Default = sets.Idle 
+	sets.Idle.Custom = sets.Idle
+	sets.aftercast.Idle = sets.Idle
+	
+
+	
+	
+	send_command('input /macro book 9;wait .1;input /macro set 1')
+end
+
